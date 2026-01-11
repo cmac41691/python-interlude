87af5eeb108c1d1e1ea98277043139d472ee37f8
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2026-01-11 09:12:13 -0500

    feat: add creat_contact helper

---
+++ drills/week3_data_structures/contact_manager.py
@@ -0,0 +1,10 @@
+def create_contact(name, phone, email):
+    """
+    Creates and returns a contact dictionary.
+    """
+    return {
+        "name": name,
+        "phone": phone,
+        "email": email      
+    }
+
